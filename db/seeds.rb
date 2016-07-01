b2ec01b5fbd4f464cc993d68937faa59b7c5c0a1
John Hossler <jmhossler@crimson.ua.edu>
2016-07-01 10:37:56 -0400

    Did so much stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+questions = Array.new
+
+File.open('questions.txt', 'r') do |f|
+  f.each_line do |line|
+    questions << line.split('|')
+  end
+end
+
+questions.each do |question, ans1, ans2|
+  Question.create(test: question, ans1: ans1, ans2: ans2)
+end
